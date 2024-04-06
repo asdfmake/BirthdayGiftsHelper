45dc9a006f216f1e3fec44f18300e528754e48ee
asdfmake <makevic2003@gmail.com>
2024-04-06 14:40:48 +0200

    Initial commit

---
+++ src/main/kotlin/org/ajvn/BirthdayGiftsHelper/BirthdayGiftsHelperApplication.kt
@@ -0,0 +1,11 @@
+package org.ajvn.BirthdayGiftsHelper
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BirthdayGiftsHelperApplication
+
+fun main(args: Array<String>) {
+	runApplication<BirthdayGiftsHelperApplication>(*args)
+}
---
+++ src/test/kotlin/org/ajvn/BirthdayGiftsHelper/BirthdayGiftsHelperApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ajvn.BirthdayGiftsHelper
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BirthdayGiftsHelperApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
